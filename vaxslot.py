7004037d104eb444b7a09e20fe7bb96becbfbb66
harsh <hnavadiya1802@gmail.com>
2021-09-05 10:26:43 +0530

    Removed date and pincode input & date changes automatically.

--- vaxslot.py
+++ vaxslot.py
@@ -2,15 +2,16 @@ import requests
 import telegram_send
 import time
 import pyautogui
+from datetime import datetime , timedelta
 
 
-pincode = pyautogui.prompt('Enter the pincode')
-date = pyautogui.prompt('Enter the Date in DD-MM-YYYY format')
+# pincode = pyautogui.prompt('Enter the pincode')
+# date = pyautogui.prompt('Enter the Date in DD-MM-YYYY format')
 
 
 
-# pincode = 395004
-# date = '31-08-2021'
+pincode = 395004
+date = datetime.now() + timedelta(1)
 base = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}'.format(pincode,date)
 list = []
 
@@ -22,16 +23,26 @@ def check():
 	page = requests.get(base)
 	page_json = page.json()
 	data = page_json['sessions']
+
 	# res = len([ele for ele in data if isinstance(ele, dict)])
+
+
+
+
+# Katargam community hall : 709997
+# samast patidar samaj vadi : 711599
+# singanpore community hall : 70
+
 
 	for i in data:
 		name = i['name']
+		center = i['center_id']
 		address = i['address']
 		dose1 = i['available_capacity_dose1']
 		dose2 = i['available_capacity_dose2']
 		doseboth = i['available_capacity']
 
-		if ((i['available_capacity'] > 0) & (i['fee_type'] == 'Free')):
+		if ((doseboth > 0) & (center == 711599)):
 			msg = 'Hey, I found slot.\n {}\n {}\n Dose 1:{}\n Dose 2:{}'.format(name,address,dose1,dose2)
 			counter += 1
 			telegram_send.send(messages=[msg])
